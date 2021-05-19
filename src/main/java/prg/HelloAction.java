b08db36c6ed8182397f822213b5cf43bc8834c81
Gino Palo <ginopalo@gmail.com>
2021-05-19 09:07:26 +0200

    initial

---
+++ src/main/java/prg/HelloAction.java
@@ -0,0 +1,14 @@
+package prg;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * <code>Set welcome message.</code>
+ */
+public class HelloAction extends ActionSupport {
+
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+
+}
